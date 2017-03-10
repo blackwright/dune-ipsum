222df9c8f7c6374d366b7bafb021b67c49218ab7
Jerry Gao <gao@blackwright.com>
2017-03-10 12:08:31 -0500

    add resize listener for mobile background jump issue

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,22 @@
-var DUNE = {
-  init: () => {
-    new Clipboard('button');
-  }
-};
+var DUNE = DUNE || {};
 
-document.addEventListener("DOMContentLoaded", () => {
-  DUNE.init();
+DUNE.InitModule = (function() {
+  
+  let init = () => {
+    new Clipboard('button');
+    $(window).resize(_resizeBackground);
+  };
+
+  let _resizeBackground = () => {
+    $('body').height( $(window).height() + 60);
+  };
+
+  return {
+    init: init
+  };
+
+})();
+
+$(document).ready( () => {
+  DUNE.InitModule.init();
 });
